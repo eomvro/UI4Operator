10eda51493d6b5758efca36464c95eb22a325fef
eomvro <eomvr0526@naver.com>
2022-03-25 17:46:57 +0900

    UI 연결 작업

--- OperatorUI/socket_comm_tkinter.py
+++ OperatorUI/socket_comm_tkinter.py
@@ -1,6 +1,10 @@
 import socket
 from tkinter import *
 import threading
+from tkinter import ttk
+
+from click import style
+from matplotlib import image
 
 # soc = socket.socket()
 # soc.connect(('localhost', 1234))
@@ -8,52 +12,201 @@ import threading
 BG_GRAY = "thistle"
 BG_COLOR = "lavenderblush"
 EN_COLOR = "whitesmoke"
-TEXT_COLOR = 'black'
+TEXT_COLOR = "black"
 
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
 
-class ChatApplication:
-    ip = 'localhost'
+
+class ChatApplication(Tk):
+    ip = "192.168.50.173"
     port = 1234
 
     def __init__(self):
-        self.conn_soc = None
-        self.window = None
-        self.text_widget=None
-        self.msg_entry=None
-
-    def run(self):
+        Tk.__init__(self)
         self.conn_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.conn_soc.connect((ChatApplication.ip, ChatApplication.port))
-        self.window = Tk()
-        self._setup_main_window()
-        th = threading.Thread(target=self.update_conversation)
-        th.start()
-        self.window.mainloop()
-
-    def _setup_main_window(self):
-        self.window.title("Chat")
-        self.window.resizable(width=False, height=False)
-        self.window.configure(width=470, height=550, bg=BG_COLOR)
+        self.curFrame = None
+        self._frameA = Avatar("avatarA", self)
+        self._frameB = Avatar("avatarB", self)
+        self.title("Chat")
+        self.geometry("470x600+100+100")
+        self.resizable(False, False)
 
         # head label
-        head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
-                           text="용두산 공원 챗봇", font=FONT_BOLD, pady=10)
-        head_label.place(relwidth=1)
+        head_label = Label(
+            self,
+            bg=BG_COLOR,
+            fg=TEXT_COLOR,
+            text="용두산 공원 챗봇",
+            font=FONT_BOLD,
+            pady=10,
+            width=470,
+        )
+        # head_label.place(relwidth=1)
+        head_label.pack(side="top")
+        selectAvatar = Label(self, bg="gray", fg=TEXT_COLOR, pady=10, width=470)
+        selectAvatar.pack(side="top")
 
-        # tiny divider
-        line = Label(self.window, width=450, bg=BG_GRAY)
-        line.place(relwidth=1, rely=0.07, relheight=0.012)
+        # select button
+        self.avatarA = Button(
+            selectAvatar,
+            text="avatarA",
+            command=lambda: self.switch_frame(self._frameA),
+        )
+        self.avatarB = Button(
+            selectAvatar,
+            text="avatarB",
+            command=lambda: self.switch_frame(self._frameB),
+        )
 
-        # text widget
-        self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
-                                font=FONT, padx=5, pady=5)
-        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
+        self.avatarA.place(relx=0, rely=0.1, relheight=1, relwidth=0.22)
+        self.avatarB.place(relx=0.2, rely=0.1, relheight=1, relwidth=0.22)
+
+        self.switch_frame(self._frameA)
+
+        th = threading.Thread(target=self.update_conversation)
+        th.start()
+
+    def switch_frame(self, selected_frame):
+        if self.curFrame is not None:
+            self.curFrame.place_forget()
+        self.curFrame = selected_frame
+        self.curFrame.place(relx=0, rely=0.13, relheight=0.87, relwidth=1)
+        if self.curFrame == self._frameA:
+            self.avatarA.configure(bg=BG_COLOR)
+            self.avatarB.configure(bg=BG_GRAY)
+        else:
+            self.avatarA.configure(bg=BG_GRAY)
+            self.avatarB.configure(bg=BG_COLOR)
+
+    def alarm(self):
+        if self.curFrame == self._frameA:
+            self.avatarA.configure(bg="red")
+        else:
+            self.avatarB.configure(bg="red")
+
+    def update_conversation(self):
+        while True:
+            msg = self.conn_soc.recv(1024).decode()
+            if not msg:
+                return
+            self.avatarA.configure(bg="red")
+            # self._frameA.msg_entry.delete(0, END)
+            msg1 = f"{msg}\n"
+            self._frameA.text_widget.configure(state=NORMAL)
+            self._frameA.text_widget.insert(END, msg1)
+            self._frameA.text_widget.configure(state=DISABLED)
+            self._frameA.text_widget.see(END)
+
+    # def __init__(self):
+    #     self.conn_soc = None
+    #     self.window = None
+    #     self.text_widget = None
+    #     self.msg_entry = None
+
+    # def run(self):
+    #     self.conn_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     self.conn_soc.connect((ChatApplication.ip, ChatApplication.port))
+    #     self.window = Tk()
+    #     self._setup_main_window()
+
+    #     self.window.mainloop()
+
+    # def _setup_main_window(self):
+    #     self.window.title("Chat")
+    #     self.window.resizable(width=False, height=False)
+    #     self.window.configure(width=470, height=550, bg=BG_COLOR)
+
+    #     # head label
+    #     head_label = Label(
+    #         self.window,
+    #         bg=BG_COLOR,
+    #         fg=TEXT_COLOR,
+    #         text="용두산 공원 챗봇",
+    #         font=FONT_BOLD,
+    #         pady=10,
+    #     )
+    #     head_label.place(relwidth=1)
+
+    #     # tiny divider
+    #     line = Label(self.window, width=450, bg=BG_GRAY)
+    #     line.place(relwidth=1, rely=0.07, relheight=0.012)
+
+    #     # text widget
+    #     self.text_widget = Text(
+    #         self.window,
+    #         width=20,
+    #         height=2,
+    #         bg=BG_COLOR,
+    #         fg=TEXT_COLOR,
+    #         font=FONT,
+    #         padx=5,
+    #         pady=5,
+    #     )
+    #     self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
+    #     self.text_widget.configure(cursor="arrow", state=DISABLED)
+
+    #     # bottom label
+    #     bottom_label = Label(self.window, bg=BG_GRAY, height=80)
+    #     bottom_label.place(relwidth=1, rely=0.825)
+
+    #     # message entry box
+    #     self.msg_entry = Entry(bottom_label, bg=EN_COLOR, fg=TEXT_COLOR, font=FONT)
+    #     self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+    #     self.msg_entry.focus()
+    #     self.msg_entry.bind("<Return>", self._on_enter_pressed)
+
+    #     # send button
+    #     send_button = Button(
+    #         bottom_label,
+    #         text="send",
+    #         font="FONT_BOLD",
+    #         width=20,
+    #         bg=BG_GRAY,
+    #         command=lambda: self._on_enter_pressed(None),
+    #     )
+    #     send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+
+    # def _on_enter_pressed(self, event):
+    #     msg = self.msg_entry.get()
+    #     self._insert_message(msg)
+
+    # def _insert_message(self, msg):
+    #     if not msg:
+    #         return
+
+    #     self.msg_entry.delete(0, END)
+
+    #     msg1 = f"You : {msg}\n"
+    #     self.text_widget.configure(state=NORMAL)
+    #     self.text_widget.insert(END, msg1)
+    #     self.text_widget.configure(state=DISABLED)
+
+    #     self.text_widget.see(END)
+    #     self.conn_soc.send(bytes(msg, "utf-8"))
+
+
+class Avatar(Frame):
+    def __init__(self, name, app):
+        self.app = app
+        super(Avatar, self).__init__()
+        self.name = name
+        self.text_widget = Text(
+            self,
+            width=20,
+            height=2,
+            bg=BG_COLOR,
+            fg=TEXT_COLOR,
+            font=FONT,
+            padx=5,
+            pady=5,
+        )
+        self.text_widget.place(relheight=0.825, relwidth=1)
         self.text_widget.configure(cursor="arrow", state=DISABLED)
 
         # bottom label
-        bottom_label = Label(self.window, bg=BG_GRAY, height=80)
+        bottom_label = Label(self, bg=BG_GRAY, height=80)
         bottom_label.place(relwidth=1, rely=0.825)
 
         # message entry box
@@ -63,19 +216,23 @@ class ChatApplication:
         self.msg_entry.bind("<Return>", self._on_enter_pressed)
 
         # send button
-        send_button = Button(bottom_label, text="send", font="FONT_BOLD", width=20, bg=BG_GRAY,
-                             command=lambda: self._on_enter_pressed(None))
+        send_button = Button(
+            bottom_label,
+            text="send",
+            font="FONT_BOLD",
+            width=20,
+            bg=BG_GRAY,
+            command=lambda: self._on_enter_pressed("<Retrun>"),
+        )
         send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
-
-    def _on_enter_pressed(self, event):
-        msg = self.msg_entry.get()
-        self._insert_message(msg)
 
     def _insert_message(self, msg):
         if not msg:
             return
 
         self.msg_entry.delete(0, END)
+        if msg == "new":
+            self.app.alarm()
 
         msg1 = f"You : {msg}\n"
         self.text_widget.configure(state=NORMAL)
@@ -83,20 +240,12 @@ class ChatApplication:
         self.text_widget.configure(state=DISABLED)
 
         self.text_widget.see(END)
-        self.conn_soc.send(bytes(msg, 'utf-8'))
+        app.conn_soc.send(bytes(msg, "utf-8"))
 
-    def update_conversation(self):
-        while True:
-            msg = self.conn_soc.recv(1024).decode()
-            if not msg:
-                return
+    def _on_enter_pressed(self, event):
+        msg = self.msg_entry.get()
+        self._insert_message(msg)
 
-            self.msg_entry.delete(0, END)
-            msg1 = f"{msg}\n\n"
-            self.text_widget.configure(state=NORMAL)
-            self.text_widget.insert(END, msg1)
-            self.text_widget.configure(state=DISABLED)
-            self.text_widget.see(END)
 
 # def gettext():
 #     msg = soc.recv(1024).decode()
@@ -104,7 +253,7 @@ class ChatApplication:
 
 if __name__ == "__main__":
     app = ChatApplication()
-    app.run()
+    app.mainloop()
 
 ###0207 오후 4시 반 현재 상황
 ###첫 대화는 오는데 그 후 대화 업데이트가 안됨
--- socket_comm_tkinter.py
+++ socket_comm_tkinter.py
@@ -1,17 +1,19 @@
 import socket, threading
 from makeanswer2 import makeanswer
 from eunjeon import Mecab
+
 mecab = Mecab()
 
 ser = socket.socket()
-ser.bind(("localhost", 1234))
+ser.bind(("192.168.50.173", 1234))
 ser.listen(3)
 
 c, adress = ser.accept()
 
+
 def decode_str(sentence):
-    result = ''
-    tks = str(sentence).split('##')
+    result = ""
+    tks = str(sentence).split("##")
     for tk in tks:
         try:
             idx = int(tk)
@@ -20,46 +22,40 @@ def decode_str(sentence):
             result += tk
     return result
 
-print('대기중')
+
+print("대기중")
+
 
 def binder(client_socket, addr):
     try:
         while True:
             print("connected_operatorUI")
-            # data = client_socket.recv(4)
-            # length = int.from_bytes(data, "big")
-            # data = client_socket.recv(length)
-            # msg = data.decode()
-            # decoded_msg = decode_str(msg)
-            # question = decoded_msg[1:]
-            #
-            # answer = makeanswer(question)
+            data = client_socket.recv(4)
+            length = int.from_bytes(data, "big")
+            data = client_socket.recv(length)
+            msg = data.decode()
+            decoded_msg = decode_str(msg)
+            question = decoded_msg[1:]
 
+            answer = makeanswer(question)
 
-            '''
-                        if answer.__contains__('-1'):
-                answer = '***대답해주세요***'
-                msg = '***대답해주세요***\n' + 'User     : ' + question
-                #msg = answer
+            if answer.__contains__("-1"):
+                answer = "***대답해주세요***"
+                msg = "***대답해주세요***\n" + "User     : " + question
 
             else:
                 answer = answer
-                msg = 'User     : ' + question + '\n\n' + 'Chatbot : ' + answer + '\n\n'
-                #msg = answer
-            '''
+                msg = "User     : " + question + "\n\n" + "Chatbot : " + answer + "\n\n"
 
-            # msg = 'User     : ' + question + '\n\n' + 'Chatbot : ' + answer + '\n\n'
-            #
-            # print('client : ' + c.recv(1024).decode())
-            msg = input("you : ")
-            c.send(bytes(msg, 'utf-8'))
-            # data = answer.encode()
-            # length = len(data)
-            # client_socket.sendall(data)
-            # client_socket.sendall(length.to_bytes(4, byteorder='little'))
+            msg = "User     : " + question + "\n\n" + "Chatbot : " + answer + "\n\n"
+
+            data = answer.encode()
+            length = len(data)
+            client_socket.sendall(data)
+            client_socket.sendall(length.to_bytes(4, byteorder="little"))
 
             #####
-            c.send(bytes(msg, 'utf-8'))
+            c.send(bytes(msg, "utf-8"))
 
     except:
         print("except : ", addr)
@@ -67,19 +63,21 @@ def binder(client_socket, addr):
     finally:
         client_socket.close()
 
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('', 8888))
+server_socket.bind(("", 8888))
 server_socket.listen()
 
 try:
     while True:
-        # client_socket, addr = ser.accept()
-        # th = threading.Thread(target=binder, args=(client_socket, addr))
-        # th.start()
-        # print('client : ' + c.recv(1024).decode())
-        msg = input("you : ")
-        c.send(bytes('avatar: '+msg, 'utf-8'))
+        client_socket, addr = ser.accept()
+        th = threading.Thread(target=binder, args=(client_socket, addr))
+        th.start()
+        print("client : " + c.recv(1024).decode())
+        # msg = input("you : ")
+        # c.send(bytes("avatar: " + msg, "utf-8"))
+        # print("client : " + c.recv(1024).decode())
 except:
     print("server")
 
@@ -88,4 +86,5 @@ finally:
 
 ##0204 15시 현재 상황 대화 계속 이어질 수 있고 OperatorUI(tkinter program)으로 계속해서 넘어감(콘솔 창에 업데이트 ㅇ)
 ##해야할 사항 : 1. 넘어간 대화들 채팅창에서 업데이트
-            ##2. 오퍼레이터가 필요시 채팅한 내용을 입력받아 넘어올 수 있도록
+##2. 오퍼레이터가 필요시 채팅한 내용을 입력받아 넘어올 수 있도록
+
